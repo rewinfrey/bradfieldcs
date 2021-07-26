997f3e41fcb19d20d384a95e4bbc14598d5b2673
Rick Winfrey <rewinfrey@github.com>
2021-07-25 17:43:20 -0700

    Add ast subcommand and sample ast

--- languages-compilers-interpreters/rustylox/src/main.rs
+++ languages-compilers-interpreters/rustylox/src/main.rs
@@ -1,13 +1,16 @@
 #[macro_use]
 extern crate enum_display_derive;
 
-use clap::{App, Arg};
+use ast::Expr;
+use clap::{App, Arg, SubCommand};
 use error::{error, ErrorKind};
 use scanner::{default_reserved, Scanner};
 use std::fs;
 use std::io::{stdin, stdout, Write};
 use std::path::Path;
+use token::{Token, TokenType};
 
+mod ast;
 mod error;
 mod scanner;
 mod token;
@@ -68,14 +71,40 @@ fn run(source: String) {
     }
 }
 
+fn run_ast() {
+    // -123 * 45.67
+    let expr = Expr::Binary(
+        Box::new(Expr::Unary(
+            Token::new(TokenType::Minus, String::from("-"), 0, 0),
+            Box::new(Expr::Literal(Token::new(
+                TokenType::Number,
+                String::from("123"),
+                0,
+                0,
+            ))),
+        )),
+        Token::new(TokenType::Star, String::from("*"), 0, 0),
+        Box::new(Expr::Grouping(Box::new(Expr::Literal(Token::new(
+            TokenType::Number,
+            String::from("45.67"),
+            0,
+            0,
+        ))))),
+    );
+    println!("{}", expr);
+}
+
 fn main() {
     let m = App::new("rustylox")
         .version("0.0.1")
         .about("Rust interpreter for the Lox language")
         .arg(Arg::with_name("input").index(1))
+        .subcommand(SubCommand::with_name("ast"))
         .get_matches();
 
-    if m.is_present("input") {
+    if let Some(_) = m.subcommand_matches("ast") {
+        run_ast();
+    } else if m.is_present("input") {
         if let Some(file_path) = m.value_of("input") {
             std::process::exit(match run_file(Path::new(file_path)) {
                 Ok(_) => 0,

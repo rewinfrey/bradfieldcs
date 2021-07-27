f54144d92f66955dffd2499eb46f7d3f2bd49c50
Rick Winfrey <rewinfrey@github.com>
2021-07-26 17:04:08 -0700

    Evaluator returns Result<Value, ()>

--- languages-compilers-interpreters/rustylox/src/evaluator.rs
+++ languages-compilers-interpreters/rustylox/src/evaluator.rs
@@ -1,16 +1,52 @@
 use super::ast::Expr;
 use super::error::{error, ErrorKind};
+use std::fmt::Display;
 
-pub fn evaluate(ast: Expr) -> Result<f64, ()> {
+#[derive(Debug, Display)]
+pub enum Value {
+    Nil,
+    Bool(bool),
+    Number(f64),
+    String(String),
+}
+
+impl Into<Option<f64>> for Value {
+    fn into(self) -> Option<f64> {
+        match self {
+            Value::Number(n) => Some(n),
+            _ => None,
+        }
+    }
+}
+
+impl Into<Option<String>> for Value {
+    fn into(self) -> Option<String> {
+        match self {
+            Value::String(s) => Some(s),
+            _ => None,
+        }
+    }
+}
+
+impl Into<Option<bool>> for Value {
+    fn into(self) -> Option<bool> {
+        match self {
+            Value::Bool(b) => Some(b),
+            _ => None,
+        }
+    }
+}
+
+pub fn evaluate(ast: Expr) -> Result<Value, ()> {
     match ast {
         Expr::Binary(l, op, r) => {
-            let l_result = evaluate(*l);
-            let r_result = evaluate(*r);
+            let l_result: Option<f64> = evaluate(*l)?.into();
+            let r_result: Option<f64> = evaluate(*r)?.into();
             match op.lexeme.as_str() {
-                "+" => Ok(l_result? + r_result?),
-                "-" => Ok(l_result? - r_result?),
-                "*" => Ok(l_result? * r_result?),
-                "/" => Ok(l_result? / r_result?),
+                "+" => Ok(Value::Number(l_result.unwrap() + r_result.unwrap())),
+                "-" => Ok(Value::Number(l_result.unwrap() - r_result.unwrap())),
+                "*" => Ok(Value::Number(l_result.unwrap() * r_result.unwrap())),
+                "/" => Ok(Value::Number(l_result.unwrap() / r_result.unwrap())),
                 _ => {
                     error(
                         op.line,
@@ -24,12 +60,12 @@ pub fn evaluate(ast: Expr) -> Result<f64, ()> {
             }
         }
         Expr::Grouping(expr) => evaluate(*expr),
-        Expr::NumberLiteral(n) => Ok(n),
+        Expr::NumberLiteral(n) => Ok(Value::Number(n)),
         Expr::Unary(op, expr) => {
-            let unary = evaluate(*expr);
+            let unary: Option<f64> = evaluate(*expr)?.into();
             match op.lexeme.as_str() {
-                "-" => Ok(-unary?),
-                "+" => Ok(unary?),
+                "-" => Ok(Value::Number(-unary.unwrap())),
+                "+" => Ok(Value::Number(unary.unwrap())),
                 _ => {
                     error(0, 0, 0, String::from("Unknown unary operator error"), ErrorKind::EvaluatorError);
                     Err(())

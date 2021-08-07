0882fcdd0cbf1b56dd96af4d0b4e531728783a6f
Rick Winfrey <rewinfrey@github.com>
2021-08-07 13:36:38 -0700

    Extract value module

---
+++ languages-compilers-interpreters/rustylox/src/value.rs
@@ -0,0 +1,38 @@
+use std::fmt::Display;
+
+#[derive(Clone, Debug, Display)]
+pub enum Value {
+    Nil,
+    True,
+    False,
+    Number(f64),
+    String(String),
+}
+
+impl Into<Option<f64>> for Value {
+    fn into(self) -> Option<f64> {
+        match self {
+            Value::Number(n) => Some(n),
+            _ => None,
+        }
+    }
+}
+
+impl Into<Option<String>> for Value {
+    fn into(self) -> Option<String> {
+        match self {
+            Value::String(s) => Some(s),
+            _ => None,
+        }
+    }
+}
+
+impl Into<Option<bool>> for Value {
+    fn into(self) -> Option<bool> {
+        match self {
+            Value::True => Some(true),
+            Value::False => Some(false),
+            _ => None,
+        }
+    }
+}
